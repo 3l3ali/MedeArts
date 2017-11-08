7f5b3a875484fbe4aa098e88ce5737b30cdbcf06
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-11-08 21:51:00 +0200

    translation yml and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index,:show] do
-    resources :posts, only: [:new, :create, :edit, :update, :destroy]
+
+  scope '(:locale)', locale: /en|ar/ do
+    resources :users, only: [:index,:show] do
+      resources :posts, only: [:new, :create, :edit, :update, :destroy]
+    end
   end
 end
