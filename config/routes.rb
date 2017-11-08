ff4dd62f1603aa62cf63e13be34d27689a6ec10a
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-11-08 23:57:26 +0200

    i18n fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
 
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
   scope '(:locale)', locale: /en|ar/ do
+    root to: 'pages#home'
     resources :users, only: [:index,:show] do
       resources :posts, only: [:new, :create, :edit, :update, :destroy]
     end
